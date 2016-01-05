b3cbd561626281723edc2db6df590835fde1d31f
Ophélie <ophelie.delsaux@insa-lyon.fr>
2016-01-05 17:52:37 +0100

    Dév du début de l'application : Page d'inscription puis page de formulaire d'inscription. Quelques transition effectuées, design fait.

---
+++ app/src/main/java/com/kiwi/kiwi/PageInscription.java
@@ -0,0 +1,29 @@
+package com.kiwi.kiwi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+public class PageInscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_inscription);
+    }
+
+    public void boutonCreerCompteListener(View view) {
+        Log.i("debug", "Appui sur boutonCreerCompte");
+        Intent menuIntent = new Intent(this, FormulaireInscription.class);
+        startActivity(menuIntent);
+    }
+
+    public void boutonSeConnecterListener(View view) {
+        Log.i("debug", "Appui sur boutonSeConnecter");
+    }
+}
