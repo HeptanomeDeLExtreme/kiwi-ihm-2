307b8d5a03fa9e7ad12b76f8c5de8a5e870b1b0f
mchalumeau <marion.chalumeau@insa-lyon.fr>
2016-01-12 02:24:23 +0100

    Mise à jour des données profil et restaurant

--- app/src/main/java/com/kiwi/kiwi/PageResto.java
+++ app/src/main/java/com/kiwi/kiwi/PageResto.java
@@ -262,7 +262,7 @@ public class PageResto extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
 
-                    Avis nouvelAvis = new Avis((int) note.getRating(), avisUtilisateur.getText().toString(), new Ami("Dog", "Snoop", "photoutilisateur"));
+                    Avis nouvelAvis = new Avis((int) note.getRating(), avisUtilisateur.getText().toString(), new Ami("Wayne", "Bruce", "profil"));
                     resto.addAvis(nouvelAvis);
                     Intent intent = getIntent();
                     intent.putExtra(EXTRA_RESTO, resto.getNom());
